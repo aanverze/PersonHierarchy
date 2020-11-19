81acda10d0c6d41bcf18a523dac9690cf0730dbb
aanverze <andressaanverze@gmail.com>
2020-11-18 21:45:09 -0300

    up

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student extends Person{
+    private String program;
+    private int year;
+    private double feel;
+
+
+    public Student(String name, String address, String program, int year, double fee) {
+        super(name, address);
+    }
+
+
+}
